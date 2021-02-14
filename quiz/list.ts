3764cc388ec4d26d15dcac73a7e86e7e35d756a7
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-14 01:40:02 +0100

    feat: placeholder quizes

---
+++ quiz/list.ts
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    title: "CSS For Begginers",
+    image: require("../assets/images/quizes/css.png"),
+    completedQuestions: 1,
+    totalQuestions: 10,
+  },
+  {
+    id: 2,
+    title: "HTML For Begginers",
+    image: require("../assets/images/quizes/html.png"),
+    completedQuestions: 9,
+    totalQuestions: 10,
+  },
+  {
+    id: 3,
+    title: "React For Begginers",
+    image: require("../assets/images/quizes/react.png"),
+    completedQuestions: 10,
+    totalQuestions: 10,
+  },
+  {
+    id: 4,
+    title: "Vue For Begginers",
+    image: require("../assets/images/quizes/vue.png"),
+    completedQuestions: 2,
+    totalQuestions: 10,
+  },
+  {
+    id: 5,
+    title: "HTTP For Begginers",
+    image: require("../assets/images/quizes/https.png"),
+    completedQuestions: 3,
+    totalQuestions: 10,
+  },
+  {
+    id: 6,
+    title: "Javascript For Begginers",
+    image: require("../assets/images/quizes/javascript.png"),
+    completedQuestions: 5,
+    totalQuestions: 10,
+  },
+];
