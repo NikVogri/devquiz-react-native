3fee3aae31f6108e502a8429d39c213ee6bc52e9
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-14 01:41:58 +0100

    feat: awards screen

---
+++ screens/Awards.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import AwardCard from "../components/AwardCard";
+import Main from "../components/Layout/Main";
+
+export default function Awards() {
+  return (
+    <Main>
+      <ScrollView contentContainerStyle={style.awards}>
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+        <AwardCard />
+      </ScrollView>
+    </Main>
+  );
+}
+
+const style = StyleSheet.create({
+  awards: {
+    paddingTop: 20,
+    padding: 10,
+    paddingBottom: 180,
+    justifyContent: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "#5A5A5A",
+  },
+});
