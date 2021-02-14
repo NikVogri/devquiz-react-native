031fcff44e5e5940afeb7e0dff3b2ee9f24e9d59
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-14 15:28:11 +0100

    feat: button now uses correct event

--- components/StartQuizButton.tsx
+++ components/StartQuizButton.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Colors from "../constants/Colors";
 
 interface StartQuizButtonProps {
   startQuiz: () => void;
+  [object: string]: any;
 }
 
-const StartQuizButton = ({ startQuiz }: StartQuizButtonProps) => {
+const StartQuizButton = ({ startQuiz, ...props }: StartQuizButtonProps) => {
   return (
-    <View onTouchStart={startQuiz}>
-      <Text>Start Quiz</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={startQuiz}>
+      <View onTouchStart={startQuiz} {...props} style={style.button}>
+        <Text style={style.text}>Start Quiz</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const style = StyleSheet.create({
-  button: {},
-  text: {},
+  button: {
+    backgroundColor: Colors.primaryButton,
+    borderRadius: 10,
+    paddingVertical: 15,
+    width: "100%",
+    ...Colors.shadow,
+  },
+  text: {
+    color: Colors.white,
+    textAlign: "center",
+    fontSize: 24,
+    fontWeight: "400",
+  },
 });
 
 export default StartQuizButton;
