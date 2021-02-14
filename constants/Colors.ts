9762a9827bbcdc6da9cfcaa51de969a92f3c2868
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-14 01:40:36 +0100

    feat: variable colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,13 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  backgroundPrimary: "#5A5A5A",
+  backgroundDark: "#333333",
+  navigationBar: "#4F4F4F",
+  navigationItemActive: "#333333",
+  white: "#ffff",
+  black: "#0000",
+  completionBarBackground: "#E0E0E0",
+  lowCompletionBackground: "#2D9CDB",
+  mediumCompletionBackground: "#F2C94C",
+  completedBackground: "#27AE60",
+  primaryButton: "#27AE60",
 };
