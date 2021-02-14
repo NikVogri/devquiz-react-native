5c04c4de291b2d71069c7111e1c199570979f51b
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-14 01:41:51 +0100

    feat: bottom navigation bar now links correctly

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,73 +1,103 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import * as React from "react";
+
+import HomeScreen from "../screens/Home";
+import ProfileScreen from "../screens/Profile";
+import AwardsScreen from "../screens/Awards";
+import { BottomTabParamList } from "../types";
+import { StyleSheet } from "react-native";
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
-
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+      initialRouteName="home"
+      tabBarOptions={{
+        style: style.navigation,
+        showLabel: false,
+      }}
+    >
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
+        name="awards"
+        component={AwardsNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome5 name="award" size={24} color="white" />
+            ) : (
+              <FontAwesome5 name="award" size={24} color="gray" />
+            ),
+        }}
+      />
+
+      <BottomTab.Screen
+        name="home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome name="home" size={24} color="white" />
+            ) : (
+              <FontAwesome name="home" size={24} color="gray" />
+            ),
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
+        name="profile"
+        component={ProfileNavigator}
         options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <FontAwesome name="user" size={24} color="white" />
+            ) : (
+              <FontAwesome name="user" size={24} color="gray" />
+            ),
         }}
       />
     </BottomTab.Navigator>
   );
 }
 
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
-}
+const style = StyleSheet.create({
+  navigation: {
+    backgroundColor: "#4F4F4F",
+    borderTopColor: "#4F4F4F",
+  },
+});
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator<TabOneParamList>();
+const Awards = createStackNavigator();
 
-function TabOneNavigator() {
+function AwardsNavigator() {
   return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
-      />
-    </TabOneStack.Navigator>
+    <Awards.Navigator screenOptions={{ headerShown: false }}>
+      <Awards.Screen name="Awards" component={AwardsScreen} />
+    </Awards.Navigator>
   );
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const Home = createStackNavigator();
 
-function TabTwoNavigator() {
+function HomeNavigator() {
   return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
-      />
-    </TabTwoStack.Navigator>
+    <Home.Navigator screenOptions={{ headerShown: false }}>
+      <Home.Screen name="Home" component={HomeScreen} />
+    </Home.Navigator>
+  );
+}
+
+const Profile = createStackNavigator();
+
+function ProfileNavigator() {
+  return (
+    <Profile.Navigator screenOptions={{ headerShown: false }}>
+      <Profile.Screen name="Profile" component={ProfileScreen} />
+    </Profile.Navigator>
   );
 }
