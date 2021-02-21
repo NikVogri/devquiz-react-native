86bddb69e79b6ab7283f96f4e3102cd5422c24d9
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-21 22:20:32 +0100

    feat: docs

--- lib/calculatePercentage.ts
+++ lib/calculatePercentage.ts
@@ -1,3 +1,9 @@
+/**
+ * Given two numbers return rounded percentage
+ * @param num1
+ * @param num2
+ * @returns number
+ */
 const calculatePercentage = (num1: number, num2: number) => {
   const division = num1 < num2 ? num1 / num2 : num2 / num1;
   return Math.floor(division * 100);
