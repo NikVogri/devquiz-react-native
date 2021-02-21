645c222803b9fe9c19b5f5b433b2ba4e540637f5
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-21 22:21:56 +0100

    wip: placeholder awards list

---
+++ awards/list.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    quizId: 1,
+    text: "Completed CSS For Begginers Quiz",
+    date: new Date(),
+    image: require("../assets/images/quizes/css.png"),
+  },
+  {
+    id: 2,
+    quizId: 2,
+    text: "Completed HTML For Begginers Quiz",
+    date: new Date(),
+    image: require("../assets/images/quizes/html.png"),
+  },
+];
