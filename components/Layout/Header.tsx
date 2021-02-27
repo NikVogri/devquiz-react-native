912247689f147821d51954f5ccbe58170560bdff
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-27 20:06:54 +0100

    feat: header now displays coins and hearts instead of user profile

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -1,18 +1,48 @@
-import ExpoStatusBar from "expo-status-bar/build/ExpoStatusBar";
-import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 import Colors from "../../constants/Colors";
-
+import HeartContext, { MAX_HEARTS } from "../../context/HeartContext";
 const Header = () => {
+  const { hearts } = useContext(HeartContext);
+
   return (
     <View style={style.header}>
-      <View style={style.left}>
-        <Image
-          style={style.image}
-          source={require("../../assets/images/placeholder_award.png")}
+      <View style={style.iconContainer}>
+        {Array(hearts)
+          .fill(1)
+          .map((_, index) => (
+            <FontAwesome
+              name="heart"
+              size={32}
+              color="#EE3939"
+              style={style.heart}
+              key={index}
+            />
+          ))}
+
+        {Array(MAX_HEARTS - hearts)
+          .fill(1)
+          .map((_, index) => (
+            <FontAwesome
+              name="heart"
+              size={32}
+              color="#828282"
+              style={style.heart}
+              key={index}
+            />
+          ))}
+      </View>
+
+      <View style={style.iconContainer}>
+        <Text style={style.coinsText}>200</Text>
+        <FontAwesome5
+          name="coins"
+          size={18}
+          color="yellow"
+          style={style.coins}
         />
-        <Text style={style.username}>Unknown Player</Text>
-        <Text style={style.level}>Level 1</Text>
       </View>
     </View>
   );
@@ -22,39 +52,34 @@ const style = StyleSheet.create({
     backgroundColor: Colors.backgroundDark,
     borderBottomColor: Colors.borderPrimary,
     borderBottomWidth: 1,
-    height: 175,
+    height: 85,
     width: "100%",
-    zIndex: 99,
     display: "flex",
     flexDirection: "row",
-    justifyContent: "center",
-    paddingTop: 20,
-    paddingLeft: 30,
-    paddingRight: 30,
+    justifyContent: "space-between",
+    zIndex: 99,
+    paddingTop: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
     ...Colors.shadow,
   },
-  left: {
+  coinsText: {
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: "500",
+  },
+  iconContainer: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "row",
     alignItems: "center",
+    paddingBottom: 10,
   },
-  right: {
-    padding: 30,
+  heart: {
+    marginRight: 5,
   },
-  image: {
-    marginBottom: 10,
-  },
-  username: {
-    color: Colors.white,
-    fontSize: 23,
-  },
-  level: {
-    color: Colors.white,
-    fontSize: 16,
-  },
-  achievement: {
-    color: Colors.white,
-    marginBottom: 5,
+  coins: {
+    marginLeft: 5,
+    marginRight: 5,
   },
 });
 
