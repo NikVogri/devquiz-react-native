049542fbdb46cff4ee7d8e4635b6832b21a82bfb
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-28 00:56:20 +0100

    feat: added a way to email me directly

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, Linking, StyleSheet, Text, View } from "react-native";
 import Main from "../components/Layout/Main";
 import PrimaryButton from "../components/UI/PrimaryButton";
 import Colors from "../constants/Colors";
@@ -28,8 +28,26 @@ export default function Profile() {
         <PrimaryButton title="Flush Local Data" onPress={handleDataFlush} />
         <PrimaryButton
           title="Contact Support"
-          onPress={() => console.log("Contact support")}
+          onPress={() =>
+            Linking.openURL(
+              "mailto:vogrinec.nik@gmail.com?subject=Contact Support"
+            )
+          }
         />
+        <PrimaryButton
+          title="Request Quiz"
+          onPress={() =>
+            Linking.openURL(
+              "mailto:vogrinec.nik@gmail.com?subject=Quiz Request"
+            )
+          }
+        />
+      </View>
+      <View>
+        <Text style={style.textCredits}>
+          Created by Nik Vogrinec in Slovenia
+        </Text>
+        <Text style={style.text}>Version: 0.0.1</Text>
       </View>
     </Main>
   );
@@ -39,9 +57,18 @@ const style = StyleSheet.create({
   container: {
     padding: 25,
     height: Dimensions.get("window").height - 220,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
     backgroundColor: Colors.backgroundDark,
+  },
+  textCredits: {
+    color: Colors.white,
+    fontSize: 18,
+    textAlign: "center",
+    paddingBottom: 15,
+  },
+  text: {
+    color: Colors.white,
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
