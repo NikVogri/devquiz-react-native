23c670a2521110b7aefa519ce5187dfa56fa3496
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-02-28 19:45:40 +0100

    feat: out of hearts modal

---
+++ components/UI/Modal/OutOfHeartsModal.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Modal from "react-native-modal";
+import Colors from "../../../constants/Colors";
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
+import PrimaryButton from "../PrimaryButton";
+interface OutOfHeartsModalProps {
+  showModal: boolean;
+  closeModal: () => void;
+}
+
+const OutOfHeartsModal = ({ closeModal, showModal }: OutOfHeartsModalProps) => {
+  return (
+    <Modal
+      isVisible={showModal}
+      onBackButtonPress={closeModal}
+      onDismiss={closeModal}
+      onBackdropPress={closeModal}
+    >
+      <View style={style.modalInner}>
+        <Text style={style.uptitle}>Oh, no!</Text>
+        <View style={style.heartsContainer}>
+          <FontAwesome name="heart" size={42} color="gray" />
+          <FontAwesome name="heart" size={42} color="gray" />
+          <FontAwesome name="heart" size={42} color="gray" />
+          <FontAwesome name="heart" size={42} color="gray" />
+          <FontAwesome name="heart" size={42} color="gray" />
+        </View>
+
+        <Text style={style.title}>You've ran out of hearts.</Text>
+        <PrimaryButton
+          onPress={() => console.log("watching video")}
+          color={"#2D9CDB"}
+        >
+          <View style={style.btn}>
+            <FontAwesome5
+              name="video"
+              size={14}
+              color="white"
+              style={style.btnIcon}
+            />
+            <Text style={style.btnText}>Watch an ad</Text>
+          </View>
+        </PrimaryButton>
+
+        <PrimaryButton
+          onPress={() => console.log("going to store")}
+          color="#27AE60"
+        >
+          <View style={style.btn}>
+            <FontAwesome5
+              name="shopping-cart"
+              size={14}
+              color="white"
+              style={style.btnIcon}
+            />
+            <Text style={style.btnText}>Go to Store</Text>
+          </View>
+        </PrimaryButton>
+      </View>
+    </Modal>
+  );
+};
+
+const style = StyleSheet.create({
+  modalInner: {
+    backgroundColor: Colors.backgroundDark,
+    width: "100%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    padding: 20,
+    borderRadius: 10,
+  },
+  heartsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  uptitle: {
+    fontSize: 42,
+    color: Colors.white,
+    paddingVertical: 30,
+    lineHeight: 30,
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 24,
+    color: Colors.white,
+    paddingVertical: 30,
+    lineHeight: 30,
+    textAlign: "center",
+  },
+  btn: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
+  },
+  btnText: {
+    color: Colors.white,
+    fontWeight: "500",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  btnIcon: {
+    marginRight: 5,
+  },
+});
+
+export default OutOfHeartsModal;
