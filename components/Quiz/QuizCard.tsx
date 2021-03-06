538dc49c8e3d53e0462ca93dae243bb264a9509f
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-03-06 20:05:33 +0100

    feat: quiz can now have multiple overlays

--- components/Quiz/QuizCard.tsx
+++ components/Quiz/QuizCard.tsx
@@ -3,71 +3,80 @@ import { View, Image, Text, StyleSheet } from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Colors from "../../constants/Colors";
 import QuizCardCompletionBar from "./QuizCardCompletionBar";
-
+import { FontAwesome5 } from "@expo/vector-icons";
+import QuizCardOverlay, { QuizOverlay } from "./QuizCardOverlay";
 interface QuizCardProps {
-  id: any;
-  title: string;
-  image: any;
-  completedQuestions: number;
-  totalQuestions: number;
-  navigation: any;
+	id: any;
+	title: string;
+	image: any;
+	completedQuestions: number;
+	totalQuestions: number;
+	navigation: any;
+	isCompleted: boolean;
 }
 
 const QuizCard = ({
-  id,
-  title,
-  image,
-  completedQuestions,
-  totalQuestions,
-  navigation,
+	id,
+	title,
+	image,
+	completedQuestions,
+	totalQuestions,
+	isCompleted,
+	navigation,
 }: QuizCardProps) => {
-  return (
-    <View style={style.card}>
-      <TouchableWithoutFeedback
-        onPress={() => navigation.navigate("QuizIntro", { id })}
-      >
-        {completedQuestions > 0 && (
-          <QuizCardCompletionBar
-            completedQuestions={completedQuestions}
-            totalQuestions={totalQuestions}
-          />
-        )}
-        <Image style={style.image} source={image} />
-        <View style={style.titleContainer}>
-          <Text style={style.title}>{title}</Text>
-        </View>
-      </TouchableWithoutFeedback>
-    </View>
-  );
+	return (
+		<View style={style.card}>
+			<TouchableWithoutFeedback
+				onPress={() => navigation.navigate("QuizIntro", { id })}
+			>
+				{isCompleted && (
+					<QuizCardOverlay type={QuizOverlay.completed} />
+				)}
+
+				{!isCompleted && completedQuestions > 0 && (
+					<QuizCardCompletionBar
+						completedQuestions={completedQuestions}
+						totalQuestions={totalQuestions}
+						isCompleted={isCompleted}
+					/>
+				)}
+
+				<Image style={style.image} source={image} />
+				<View style={style.titleContainer}>
+					<Text style={style.title}>{title}</Text>
+				</View>
+			</TouchableWithoutFeedback>
+		</View>
+	);
 };
 
 const style = StyleSheet.create({
-  card: {
-    backgroundColor: Colors.backgroundPrimary,
-    borderRadius: 10,
-    width: 150,
+	card: {
+		backgroundColor: Colors.backgroundPrimary,
+		borderRadius: 10,
+		width: 150,
 
-    // maxHeight: 180,
-    margin: 10,
-    position: "relative",
-    ...Colors.shadow,
-  },
-  image: {
-    width: "100%",
-    height: 125,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-  },
-  titleContainer: {
-    paddingHorizontal: 5,
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-  title: {
-    width: "100%",
-    color: Colors.white,
-    textAlign: "center",
-  },
+		// maxHeight: 180,
+		margin: 10,
+		position: "relative",
+		...Colors.shadow,
+	},
+	image: {
+		width: "100%",
+		height: 125,
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10,
+	},
+	titleContainer: {
+		paddingHorizontal: 5,
+		paddingTop: 20,
+		paddingBottom: 20,
+	},
+	title: {
+		width: "100%",
+		color: Colors.white,
+		textAlign: "center",
+	},
 });
 
 export default QuizCard;
---
+++ components/Quiz/QuizCardOverlay.tsx
@@ -0,0 +1,64 @@
+import { FontAwesome5 } from "@expo/vector-icons";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Colors from "../../constants/Colors";
+
+export enum QuizOverlay {
+	completed = "completed",
+	locked = "locked",
+}
+
+interface QuizCardOverlayInterface {
+	type: QuizOverlay;
+}
+
+const QuizCardOverlay = ({ type }: QuizCardOverlayInterface) => {
+	let icon;
+	switch (type) {
+		case QuizOverlay.completed:
+			icon = (
+				<FontAwesome5
+					name="check"
+					size={46}
+					color="#4ac47f"
+					style={style.icon}
+				/>
+			);
+			break;
+		case QuizOverlay.locked:
+			icon = (
+				<FontAwesome5
+					name="lock"
+					size={46}
+					color="#BDBDBD"
+					style={style.icon}
+				/>
+			);
+			break;
+		default:
+			break;
+	}
+
+	return <View style={style.overlay}>{icon}</View>;
+};
+
+const style = StyleSheet.create({
+	overlay: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		height: 125,
+		width: "100%",
+		backgroundColor: "rgba(30, 30, 30, 0.55)",
+		zIndex: 10,
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	icon: {
+		zIndex: 20,
+		...Colors.shadow,
+	},
+});
+
+export default QuizCardOverlay;
