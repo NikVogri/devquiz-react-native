ae4eed58a0392ab7252d3f55084dad4ad505f5be
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-03-15 22:30:20 +0100

    feat: update coins when quiz is purchased

--- context/CoinContext.tsx
+++ context/CoinContext.tsx
@@ -3,85 +3,91 @@ import { useAsyncLocalStorage } from "../hooks/useAsyncLocalStorage";
 import { INITIAL_COIN_AMOUNT } from "../constants/Constants";
 
 export enum Coins {
-  refill_hearts = 50,
+	refill_hearts = 50,
+	purchase_quiz = 30,
 }
 
 interface CoinsContextInterface {
-  coins: number;
-  updateCoins: (operation: Coins) => void;
-  userHasEnoughCoins: (requestedAmount: number) => boolean;
+	coins: number;
+	updateCoins: (operation: Coins) => void;
+	userHasEnoughCoins: (requestedAmount: number) => boolean;
 }
 
 const CoinContext = createContext<CoinsContextInterface>({
-  coins: 200,
-  updateCoins: () => {},
-  userHasEnoughCoins: () => false,
+	coins: 200,
+	updateCoins: () => {},
+	userHasEnoughCoins: () => false,
 });
 
 export const CoinsProvider = ({ children }: any) => {
-  const [coins, setCoins] = useState(0);
-  const { storeData, getData } = useAsyncLocalStorage();
+	const [coins, setCoins] = useState(0);
+	const { storeData, getData } = useAsyncLocalStorage();
 
-  useEffect(() => {
-    getCoinsFromLC();
-  }, []);
+	useEffect(() => {
+		getCoinsFromLC();
+	}, []);
 
-  /**
-   * @description Fetches coins count from local storage when application first loads
-   */
-  const getCoinsFromLC = async () => {
-    try {
-      const coinsCount = await getData("coins");
-      console.log("coinsCount", coinsCount);
-      if (coinsCount === null) {
-        await storeData("coins", INITIAL_COIN_AMOUNT);
-        setCoins(INITIAL_COIN_AMOUNT);
-      } else {
-        setCoins(coinsCount);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+	/**
+	 * @description Fetches coins count from local storage when application first loads
+	 */
+	const getCoinsFromLC = async () => {
+		try {
+			const coinsCount = await getData("coins");
+			console.log("coinsCount", coinsCount);
+			if (coinsCount === null) {
+				await storeData("coins", INITIAL_COIN_AMOUNT);
+				setCoins(INITIAL_COIN_AMOUNT);
+			} else {
+				setCoins(coinsCount);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  /**
-   *
-   * @param type
-   * @param changedAmount
-   * @description sets a new coins amount (add or remove)
-   */
+	/**
+	 *
+	 * @param type
+	 * @param changedAmount
+	 * @description sets a new coins amount (add or remove)
+	 */
 
-  const updateCoins = async (operation: Coins) => {
-    let newCoinsAmount = 0;
+	const updateCoins = async (operation: Coins) => {
+		let newCoinsAmount = 0;
 
-    switch (operation) {
-      case Coins.refill_hearts:
-        newCoinsAmount = coins - Coins.refill_hearts;
-        break;
+		switch (operation) {
+			case Coins.refill_hearts:
+				newCoinsAmount = coins - Coins.refill_hearts;
+				break;
+			case Coins.purchase_quiz:
+				newCoinsAmount = coins - Coins.purchase_quiz;
+				break;
 
-      default:
-        return;
-    }
+			default:
+				return;
+		}
 
-    if (newCoinsAmount < 0) return;
+		if (newCoinsAmount < 0) return;
 
-    try {
-      await storeData("coins", newCoinsAmount);
-      setCoins(newCoinsAmount);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+		try {
+			await storeData("coins", newCoinsAmount);
+			setCoins(newCoinsAmount);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  const userHasEnoughCoins = (requestedAmount: number) => {
-    return coins - requestedAmount >= 0;
-  };
+	const userHasEnoughCoins = (requestedAmount: number) => {
+		return coins - requestedAmount >= 0;
+	};
 
-  return (
-    <CoinContext.Provider value={{ coins, updateCoins, userHasEnoughCoins }}>
-      {children}
-    </CoinContext.Provider>
-  );
+	return (
+		<CoinContext.Provider
+			value={{ coins, updateCoins, userHasEnoughCoins }}
+		>
+			{children}
+		</CoinContext.Provider>
+	);
 };
 
 export default CoinContext;
