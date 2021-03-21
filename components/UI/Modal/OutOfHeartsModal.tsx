b0b7a5f75a4706bd86ff56dd56ff95f777e400f2
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-03-21 21:27:33 +0100

    feat: OutOfHearts modal now redirects to store instead of having refill heart logic

--- components/UI/Modal/OutOfHeartsModal.tsx
+++ components/UI/Modal/OutOfHeartsModal.tsx
@@ -4,140 +4,114 @@ import Modal from "react-native-modal";
 import Colors from "../../../constants/Colors";
 import { FontAwesome, FontAwesome5 } from "@expo/vector-icons";
 import PrimaryButton from "../PrimaryButton";
-import CoinContext, { Coins } from "../../../context/CoinContext";
-import HeartContext from "../../../context/HeartContext";
+import CoinContext from "../../../context/CoinContext";
+import ModalContext, {
+	Modal as ModalEnum,
+} from "../../../context/ModalContext";
 interface OutOfHeartsModalProps {
-  showModal: boolean;
-  closeModal: () => void;
+	showModal: boolean;
+	closeModal: () => void;
 }
 
 const OutOfHeartsModal = ({ closeModal, showModal }: OutOfHeartsModalProps) => {
-  const { userHasEnoughCoins, updateCoins } = useContext(CoinContext);
-  const { refillHearts } = useContext(HeartContext);
+	const { openModal } = useContext(ModalContext);
 
-  const handleRefillHearts = async () => {
-    if (userHasEnoughCoins(Coins.refill_hearts)) {
-      try {
-        await refillHearts();
-        updateCoins(Coins.refill_hearts);
-        closeModal();
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  };
+	return (
+		<Modal
+			isVisible={showModal}
+			onBackButtonPress={closeModal}
+			onBackdropPress={closeModal}
+		>
+			<View style={style.modalInner}>
+				<Text style={style.uptitle}>Oh, no!</Text>
+				<View style={style.heartsContainer}>
+					<FontAwesome name="heart" size={42} color="gray" />
+					<FontAwesome name="heart" size={42} color="gray" />
+					<FontAwesome name="heart" size={42} color="gray" />
+					<FontAwesome name="heart" size={42} color="gray" />
+					<FontAwesome name="heart" size={42} color="gray" />
+				</View>
 
-  return (
-    <Modal
-      isVisible={showModal}
-      onBackButtonPress={closeModal}
-      onDismiss={closeModal}
-      onBackdropPress={closeModal}
-    >
-      <View style={style.modalInner}>
-        <Text style={style.uptitle}>Oh, no!</Text>
-        <View style={style.heartsContainer}>
-          <FontAwesome name="heart" size={42} color="gray" />
-          <FontAwesome name="heart" size={42} color="gray" />
-          <FontAwesome name="heart" size={42} color="gray" />
-          <FontAwesome name="heart" size={42} color="gray" />
-          <FontAwesome name="heart" size={42} color="gray" />
-        </View>
+				<Text style={style.title}>You've ran out of hearts.</Text>
 
-        <Text style={style.title}>You've ran out of hearts.</Text>
+				<PrimaryButton
+					onPress={() => console.log("watching video")}
+					color={"#2D9CDB"}
+				>
+					<View style={style.btn}>
+						<FontAwesome5
+							name="video"
+							size={14}
+							color="white"
+							style={style.btnIcon}
+						/>
+						<Text style={style.btnText}>Watch an ad</Text>
+					</View>
+				</PrimaryButton>
 
-        {userHasEnoughCoins(Coins.refill_hearts) && (
-          <PrimaryButton onPress={handleRefillHearts} color="#27AE60">
-            <View style={style.btn}>
-              <FontAwesome5
-                name="coins"
-                size={14}
-                color="white"
-                style={style.btnIcon}
-              />
-              <Text style={style.btnText}>(50) Buy hearts with coins</Text>
-            </View>
-          </PrimaryButton>
-        )}
-
-        <PrimaryButton
-          onPress={() => console.log("watching video")}
-          color={"#2D9CDB"}
-        >
-          <View style={style.btn}>
-            <FontAwesome5
-              name="video"
-              size={14}
-              color="white"
-              style={style.btnIcon}
-            />
-            <Text style={style.btnText}>Watch an ad</Text>
-          </View>
-        </PrimaryButton>
-
-        {/* <PrimaryButton
-          onPress={() => console.log("going to store")}
-          color="#27AE60"
-        >
-          <View style={style.btn}>
-            <FontAwesome5
-              name="shopping-cart"
-              size={14}
-              color="white"
-              style={style.btnIcon}
-            />
-            <Text style={style.btnText}>Go to Store</Text>
-          </View>
-        </PrimaryButton> */}
-      </View>
-    </Modal>
-  );
+				<PrimaryButton
+					onPress={() => openModal(ModalEnum.store)}
+					color="#27AE60"
+				>
+					<View style={style.btn}>
+						<FontAwesome5
+							name="shopping-cart"
+							size={14}
+							color="white"
+							style={style.btnIcon}
+						/>
+						<Text style={style.btnText}>Go to Store</Text>
+					</View>
+				</PrimaryButton>
+			</View>
+		</Modal>
+	);
 };
 
 const style = StyleSheet.create({
-  modalInner: {
-    backgroundColor: Colors.backgroundDark,
-    width: "100%",
-    marginLeft: "auto",
-    marginRight: "auto",
-    padding: 20,
-    borderRadius: 10,
-  },
-  heartsContainer: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  uptitle: {
-    fontSize: 42,
-    color: Colors.white,
-    paddingVertical: 30,
-    lineHeight: 30,
-    textAlign: "center",
-  },
-  title: {
-    fontSize: 24,
-    color: Colors.white,
-    paddingVertical: 30,
-    lineHeight: 30,
-    textAlign: "center",
-  },
-  btn: {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "row",
-    justifyContent: "center",
-    width: "100%",
-  },
-  btnText: {
-    color: Colors.white,
-    fontWeight: "500",
-    textAlign: "center",
-    fontSize: 18,
-  },
-  btnIcon: {
-    marginRight: 5,
-  },
+	modalInner: {
+		backgroundColor: Colors.backgroundDark,
+		width: "100%",
+		marginLeft: "auto",
+		marginRight: "auto",
+		padding: 20,
+		borderRadius: 10,
+	},
+	heartsContainer: {
+		display: "flex",
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	uptitle: {
+		fontSize: 42,
+		color: Colors.white,
+		paddingVertical: 30,
+		lineHeight: 30,
+		textAlign: "center",
+	},
+	title: {
+		fontSize: 24,
+		color: Colors.white,
+		paddingVertical: 30,
+		lineHeight: 30,
+		textAlign: "center",
+	},
+	btn: {
+		display: "flex",
+		alignItems: "center",
+		flexDirection: "row",
+		justifyContent: "center",
+		width: "100%",
+	},
+	btnText: {
+		color: Colors.white,
+		fontWeight: "500",
+		textAlign: "center",
+		fontSize: 18,
+	},
+	btnIcon: {
+		marginRight: 5,
+	},
 });
 
 export default OutOfHeartsModal;
