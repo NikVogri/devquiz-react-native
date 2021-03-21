1284c1086c4de1eb4270333ac3c46043ce83e372
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-03-21 21:25:37 +0100

    feat: refillHearts now checks if hearts are already full

--- context/HeartContext.tsx
+++ context/HeartContext.tsx
@@ -3,97 +3,101 @@ import { MAX_HEARTS } from "../constants/Constants";
 import { useAsyncLocalStorage } from "../hooks/useAsyncLocalStorage";
 
 export enum HeartUpdate {
-  add = "add",
-  remove = "remove",
+	add = "add",
+	remove = "remove",
 }
 
 interface HeartsContextInterface {
-  hearts: number;
-  updateHeartsCount: (
-    type: HeartUpdate,
-    changedAmount: number
-  ) => Promise<void>;
-  refillHearts: () => Promise<void>;
+	hearts: number;
+	updateHeartsCount: (
+		type: HeartUpdate,
+		changedAmount: number
+	) => Promise<void>;
+	refillHearts: () => Promise<void>;
 }
 
 const HeartContext = createContext<HeartsContextInterface>({
-  hearts: 5,
-  updateHeartsCount: async () => {},
-  refillHearts: async () => {},
+	hearts: 5,
+	updateHeartsCount: async () => {},
+	refillHearts: async () => {},
 });
 
 export const HeartsProvider = ({ children }: any) => {
-  const [hearts, setHearts] = useState(MAX_HEARTS);
-  const { storeData, getData } = useAsyncLocalStorage();
+	const [hearts, setHearts] = useState(MAX_HEARTS);
+	const { storeData, getData } = useAsyncLocalStorage();
 
-  useEffect(() => {
-    getHeartAmountFromLC();
-  }, []);
+	useEffect(() => {
+		getHeartAmountFromLC();
+	}, []);
 
-  /**
-   * @description Fetches hearts count from local storage when application first loads
-   */
-  const getHeartAmountFromLC = async () => {
-    try {
-      const heartsCount = await getData("hearts");
-      console.log("heartsCount", heartsCount);
-      if (heartsCount === null) {
-        setHearts(MAX_HEARTS);
-        await storeData("hearts", MAX_HEARTS);
-      } else {
-        setHearts(heartsCount);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  };
+	/**
+	 * @description Fetches hearts count from local storage when application first loads
+	 */
+	const getHeartAmountFromLC = async () => {
+		try {
+			const heartsCount = await getData("hearts");
+			console.log("heartsCount", heartsCount);
+			if (heartsCount === null) {
+				setHearts(MAX_HEARTS);
+				await storeData("hearts", MAX_HEARTS);
+			} else {
+				setHearts(heartsCount);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  /**
-   *
-   * @param type
-   * @param changedAmount
-   * @description sets a new hearts count value (add or remove)
-   */
+	/**
+	 *
+	 * @param type
+	 * @param changedAmount
+	 * @description sets a new hearts count value (add or remove)
+	 */
 
-  const updateHeartsCount = async (
-    type: HeartUpdate,
-    changedAmount: number
-  ) => {
-    let newHeartCount = 0;
-    if (type === HeartUpdate.add) {
-      newHeartCount =
-        hearts + changedAmount > MAX_HEARTS
-          ? MAX_HEARTS
-          : hearts + changedAmount;
-    } else {
-      newHeartCount =
-        hearts - changedAmount < 0
-          ? 0
-          : (newHeartCount = hearts - changedAmount);
-    }
+	const updateHeartsCount = async (
+		type: HeartUpdate,
+		changedAmount: number
+	) => {
+		let newHeartCount = 0;
+		if (type === HeartUpdate.add) {
+			newHeartCount =
+				hearts + changedAmount >= MAX_HEARTS
+					? MAX_HEARTS
+					: hearts + changedAmount;
+		} else if (type === HeartUpdate.remove) {
+			newHeartCount =
+				hearts - changedAmount < 0
+					? 0
+					: (newHeartCount = hearts - changedAmount);
+		}
 
-    try {
-      await storeData("hearts", newHeartCount);
-      setHearts(newHeartCount);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+		try {
+			await storeData("hearts", newHeartCount);
+			setHearts(newHeartCount);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-  /**
-   *
-   * @description refills hearts
-   */
+	/**
+	 *
+	 * @description refills hearts
+	 */
 
-  const refillHearts = async () => {
-    await updateHeartsCount(HeartUpdate.add, MAX_HEARTS);
-  };
+	const refillHearts = async () => {
+		if (hearts !== MAX_HEARTS) {
+			await updateHeartsCount(HeartUpdate.add, MAX_HEARTS);
+		}
+	};
 
-  return (
-    <HeartContext.Provider value={{ hearts, updateHeartsCount, refillHearts }}>
-      {children}
-    </HeartContext.Provider>
-  );
+	return (
+		<HeartContext.Provider
+			value={{ hearts, updateHeartsCount, refillHearts }}
+		>
+			{children}
+		</HeartContext.Provider>
+	);
 };
 
 export default HeartContext;
