48fc21ff386c822f994b9fa5561e0e8f73280e99
NikVogrinecMotiviti <55229210+NikVogri@users.noreply.github.com>
2021-03-21 21:25:01 +0100

    feat: product item not has a type

--- store/store.ts
+++ store/store.ts
@@ -1,16 +1,20 @@
-import { MAX_HEARTS } from "../constants/Constants";
-
 export enum Currency {
 	coins = "coins",
 	usd = "usd",
+}
+
+export enum ProductType {
+	refill_hearts = "refill_hearts",
+	purchase_gold_coins = "purchase_gold_coins",
 }
 export interface Product {
 	id: number;
 	title: string;
 	image: any;
 	currency: Currency;
-    description: string;
+	description: string;
 	price: number;
+	type: ProductType;
 }
 
 export default [
@@ -22,13 +26,15 @@ export default [
 		description:
 			"Purchase 200 gold coins that you can use to unlock quizes and refill hearts.",
 		price: 200,
+		type: ProductType.purchase_gold_coins,
 	},
 	{
 		id: 2,
 		title: "Refill hearts",
 		image: require("../assets/images/products/hearts.png"),
-		description: `Refill ${MAX_HEARTS} hearts once.`,
+		description: `Refills your hearts.`,
 		currency: Currency.coins,
 		price: 200,
+		type: ProductType.refill_hearts,
 	},
 ];
